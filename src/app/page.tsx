902d8c1892dd653362af9458e7123c467cea65e0
Jonas Boone <boone.jonas@outlook.be>
2023-06-14 13:06:53 +0200

    fix window undefined error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import DesktopPageContent from './components/DesktopPageContent'
 import DesktopLayout from './components/desktop/DesktopLayout'
 import MobilePageContent from './components/MobilePageContent'
 import MobileLayout from './components/mobile/MobileLayout'
+import { useMediaQuery } from 'usehooks-ts'
 
 export default function Home() {
   // if this page is a client-side rendered component we cannot access the headers to retrieve the user-agent
-  // but we can use the window object to retrieve the user-agent from the navigator object
-  // the client-side components are pre-rendered on the server and the user-agent is not available at that point
-  // so we need to add a check
-  const userAgent = typeof window !== undefined && window.navigator.userAgent || '';
-  const isMobile = Boolean( userAgent!.match( /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i ));
+  // but we can use the useMediaQuery hook to determine the screen size
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
     <main>
