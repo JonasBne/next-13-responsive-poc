74be4c9b04f0635478f988fe3c2c7da997b8b794
Jonas Boone <boone.jonas@outlook.be>
2023-06-14 11:45:07 +0200

    working resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ import MobileLayout from './components/mobile/MobileLayout'
 export default function Home() {
   // if this page is a client-side rendered component we cannot access the headers to retrieve the user-agent
   // but we can use the window object to retrieve the user-agent from the navigator object
-  const userAgent = window.navigator.userAgent;
+  // the client-side components are pre-rendered on the server and the user-agent is not available at that point
+  // so we need to add a check
+  const userAgent = typeof window !== undefined && window.navigator.userAgent || '';
   const isMobile = Boolean( userAgent!.match( /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i ));
 
   return (
